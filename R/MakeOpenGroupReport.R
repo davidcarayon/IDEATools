63d39b6374c1c012653e7ef3a3d7239a1c7f4a0d
David Carayon <david.carayon@inrae.fr>
2020-11-16 10:53:09 +0100

    new ODT reports

---
+++ R/MakeOpenGroupReport.R
@@ -0,0 +1,60 @@
+#' Render a ODT report of an IDEA diagnosis
+#'
+#' @param input a system path leading either to a single file or a directory. If the input is a single file, accepted formats are : .xls, .xlsx and .json.
+#' @param output_dir the output directory
+#' @param silent Should the algorithm be silent ?
+#'
+#' @return A ODT report
+#' @import rmarkdown
+#' @import tidyr
+#' @import dplyr
+#' @import stringr
+#' @import tibble
+#' @import knitr
+#' @import ggplot2
+#' @import cli
+#' @export
+#'
+#' @examples
+#' library(IDEATools)
+#' path <- system.file("", package = "IDEATools")
+#' MakeOpenGroupReport(path)
+MakeOpenGroupReport <- function(input, output_dir = getwd(), file = "Rapport_groupe", silent = FALSE) {
+
+
+  Encoding(input) <- "UTF-8"
+
+  if (!dir.exists(output_dir)) {
+    dir.create(output_dir)
+  }
+
+  IDEAdata <- importIDEA(input)
+  outdir <- tempdir()
+
+  template <- system.file("report/rapport_groupe_open.Rmd", package = "IDEATools")
+  style_folder <- system.file("report/bandeau.png", package = "IDEATools")
+  template_folder <- system.file("report/template.odt", package = "IDEATools")
+
+  output_file <- paste0(file,"_",Sys.Date(),".odt")
+
+  # Définition des paramètres pour le rendu
+  params <- list(data = IDEAdata,
+                 outdir = outdir,
+                 anon = FALSE)
+
+  # Rendu du document dans un sous-environnement isolé
+  suppressWarnings(render(template, output_file = output_file, output_dir = output_dir,
+                          params = params,
+                          envir = new.env(parent = globalenv()), quiet = FALSE))
+
+
+  if(!silent){
+    cat_bullet(paste0("Le rapport a été exporté à l'adresse '",file.path(output_dir,output_file)), bullet = "info", bullet_col = "green")
+  }
+
+
+
+
+
+
+  }
